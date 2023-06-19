3686b81d02afffd23dfa40ad1a9fd4b3bb2943db
Serge Gori <sglemac@free.fr>
2023-06-19 16:15:40 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftUI-Pickers
+
+A description of this package.
---
+++ Sources/SwiftUI-Pickers/SwiftUI_Pickers.swift
@@ -0,0 +1,6 @@
+public struct SwiftUI_Pickers {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
